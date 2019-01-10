d1693c6bb4c399a1a93c46ec5e968c395a220b3c
yulin Wang <koumu@126.com>
2019-01-11 07:56:33 +0800

    2019-1-112019-1-112019-1-112019-1-112019-1-112019-1-112019-1-112019-1-112019-1-112019-1-112019-1-11

---
+++ common/config/main.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => 'zdl', 'httpOnly' => true],
+            //             'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+        //             'name' => 'advanced-frontend',
+            'name' => 'advanced-zdl',
+        ],
+    ],
+];
